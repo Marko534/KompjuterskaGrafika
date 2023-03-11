750feb2ae4a75b0bf774871122cf66184ebc55f5
Marko Ilioski <markoilioski21@gmail.com>
2023-03-11 19:10:58 +0100

    Test with indices values entered

--- src/main.cpp
+++ src/main.cpp
@@ -100,20 +100,20 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices_a[] = {
+            //center
+            0.0, -0.28, 0.0,
             //first
             -0.3, 0.24, 0.0,
             0.0, 0.76, 0.0,
             0.3, 0.24, 0.0,
-            0.0, -0.28, 0.0,
-            //second
-            0.3, -0.8, 0.0,
-            0.6, -0.28, 0.0,
-            0.9, -0.8, 0.0,
-            //last
-            -0.6, -0.28, 0.0,
-            -0.9, -0.8, 0.0,
-            -0.3, -0.8, 0.0,
-            0.0, -0.28, 0.0,
+//            //second
+//            0.3, -0.8, 0.0,
+//            0.6, -0.28, 0.0,
+//            0.9, -0.8, 0.0,
+//            //last
+//            -0.6, -0.28, 0.0,
+//            -0.9, -0.8, 0.0,
+//            -0.3, -0.8, 0.0,
     };
 
     std::vector<float> vertices;
@@ -122,9 +122,15 @@ int main()
         vertices.push_back(coord);
     }
 
-    unsigned int indices[] = {  // note that we start from 0!
-
+    unsigned int indices_a[] = {  // note that we start from 0!
+        0,1,3,
+        1,2,3
     };
+
+    std::vector<float> indices;
+    for (auto coord : indices_a) {
+        indices.push_back(coord);
+    }
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -133,10 +139,10 @@ int main()
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices[0]), &vertices[0], GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(indices[0]), &indices[0], GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
@@ -166,9 +172,10 @@ int main()
         // draw our first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-        glDrawArrays(GL_TRIANGLE_STRIP, 3, 4);
-        glDrawArrays(GL_TRIANGLE_FAN, 7, 4);
+//        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+//        glDrawArrays(GL_TRIANGLE_STRIP, 3, 4);
+//        glDrawArrays(GL_TRIANGLE_FAN, 7, 4);
+        glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, nullptr);
         // glBindVertexArray(0); // no need to unbind it every time
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
