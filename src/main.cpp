99d309688e6a102f57477eef7c68aa14ba7bc8af
Marko Ilioski <markoilioski21@gmail.com>
2023-03-11 19:00:13 +0100

    Test with indices

--- src/main.cpp
+++ src/main.cpp
@@ -122,15 +122,35 @@ int main()
         vertices.push_back(coord);
     }
 
-    unsigned int VBO, VAO;
+    unsigned int indices[] = {  // note that we start from 0!
+            0, 1, 2,  // first Triangle
+            1, 2, 3,   // second Triangle
+            0, 4,3,
+            0, 1, 2,  // first Triangle
+            1, 2, 3,   // second Triangle
+            0, 4,3,
+            0, 1, 2,  // first Triangle
+            1, 2, 3,   // second Triangle
+            0, 4,3,
+            0, 1, 2,  // first Triangle
+            1, 2, 3,   // second Triangle
+            0, 4,3,
+            0, 1, 2,  // first Triangle
+            1, 2, 3,   // second Triangle
+            0, 4,3,
+    };
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_a), vertices_a, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
@@ -138,14 +158,12 @@ int main()
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0);
-
-
-    // uncomment this call to draw in wireframe polygons.
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
