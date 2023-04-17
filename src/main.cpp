a0d8af101dce3cc39c768d676d6a6578e579c905
Marko Ilioski <markoilioski21@gmail.com>
2023-04-17 13:17:35 +0200

    Made the triangle bigger.

--- src/main.cpp
+++ src/main.cpp
@@ -63,9 +63,9 @@ int main() {
 
     float vertices[] = {
             // positions                   // texture coordinates
-            1.0f, -1.0f, 0.0f, 1.0f, 0.0f, // bottom right
-            0.0f, 1.0f, 0.0f, 0.5f, 1.0f, // top
-            -1.0f, -1.0f, 0.0f, 0.0f, 0.0f  // bottom left
+            1.0f, -1.0f, 0.0f, // bottom right
+            0.0f, 1.0f, 0.0f, // top
+            -1.0f, -1.0f, 0.0f  // bottom left
     };
 
     unsigned int VBO, VAO;
