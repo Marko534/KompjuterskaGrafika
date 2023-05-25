ccbe1fc190e01b7b45c3fd876dc63e2711ea829a
Marko Ilioski <markoilioski21@gmail.com>
2023-05-25 12:39:32 +0200

    New transparent image

--- src/main.cpp
+++ src/main.cpp
@@ -115,7 +115,7 @@ int main() {
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-    unsigned char *data = stbi_load("../res/textures/container.jpg", &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load("../res/textures/PacMan.png", &width, &height, &nrChannels, 0);
     if (data) {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
