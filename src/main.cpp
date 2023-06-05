3b83144d03d04752dbae685cbe934d3e6907f64e
Marko Ilioski <markoilioski21@gmail.com>
2023-06-05 14:03:29 +0200

    the cube is done

--- src/main.cpp
+++ src/main.cpp
@@ -120,7 +120,7 @@ int main() {
 
         // render
         // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // activate shader
@@ -135,7 +135,7 @@ int main() {
                             glm::vec3(0.5f, 1.0f, 0.0f));
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         projection =
-                glm::perspective(glm::radians(45.0f),
+                glm::perspective(glm::radians(70.0f),
                                  (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 100.0f);
         // retrieve the matrix uniform locations
         unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
@@ -151,7 +151,6 @@ int main() {
         glBindVertexArray(VAO);
         ourShader.passColor3("COLOR", new float[] {0.192,0.192,0.514});
         glDrawArrays(GL_TRIANGLE_FAN, 0, 8);
-        ourShader.passColor3("COLOR", new float[] {1.0,0.0,0.0});
         glDrawArrays(GL_TRIANGLE_FAN, 8, 8);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
